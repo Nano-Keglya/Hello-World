da90ef644f47b323756f3cdcefcb75094bc508be
Nano-Keglya <43568377+Nano-Keglya@users.noreply.github.com>
2020-02-03 18:37:56 +0700

    Create hello_world.cs

---
+++ hello_world.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSHARP_STARTED
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+        }
+    }
+}
